19875e0621d7f8bc36a8db2411197c966d318aa6
Ayo <heyayomideadebisi@gmail.com>
2024-02-02 21:19:07 +0100

    refactor: remove unused file

--- src/pages/api/tryEmail.json.js
+++
@@ -1,7 +0,0 @@
-import { Resend } from "resend";
-
-const resend = new Resend(import.meta.env.RESEND_API_KEY);
-
-export async function GET({ params, request }) {
-    return new Response(JSON.stringify("helo there"))
-}
