aa8d6de25adf20c828f48277efbb66c2a6172440
Ayothegod <heyayomideadebisi@gmail.com>
2024-09-07 15:00:18 +0100

    add initial tools

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import db from '@astrojs/db';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), tailwind(), react(), db()]
+});
